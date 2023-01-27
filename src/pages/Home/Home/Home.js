708b39a215b50bf2da967b742f28eae22cc775f1
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-01-28 00:31:19 +0600

    Trending page is added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Trending from "../Trending/Trending";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Trending></Trending>
     </div>
   );
 };
---
+++ src/pages/Home/Trending/TrendingCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TrendingCard = ({ data }) => {
+    const { img, title } = data.blog_info
+    return (
+        <div className="card h-[440px] bg-base-100 shadow-xl mr-4">
+            <figure><img className='img-fluid w-full' src={img} alt="img" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TrendingCard;
