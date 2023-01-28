7bbf91780bb1d31af4d815c2c884a1f443572c98
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-01-28 18:52:21 +0600

    blog page problem solved

--- src/pages/Home/Blogs/Blog/Blog.js
+++ src/pages/Home/Blogs/Blog/Blog.js
@@ -4,12 +4,14 @@ const Blog = ({ data }) => {
 
     const { blog_info } = data;
     const { img, description, title } = blog_info
+
+
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl my-10">
-            <figure><img src={img} alt="img" /></figure>
+            <figure className='h-[200px]'><img className='object-contain' src={img} alt="img" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p>{description}</p>
+                <p className='text-justify'>{description.slice(0, 250) + '...'}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Details</button>
                 </div>
